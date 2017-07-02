ca1392021c869d2d5e0b45610fbfaec822d45ebb
Dirk B <dirk.baigent@example.com>
2017-07-02 09:57:42 +0200

    SoMe_Stats v1

---
+++ js/main.js
@@ -0,0 +1,61 @@
+var ctx = document.getElementById('traffic_chart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["Mon", "Tus", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [{
+            backgroundColor: '#B4527F',
+            borderColor: '#B42867',
+            data: [0.5, 10.45, 1.10, 2.5, 8.2, 6.20, 8.30],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+
+    }
+});
+
+var ctx = document.getElementById('daily_traffic_chart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ["Mon", "Tus", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        datasets: [{
+            backgroundColor: '#2A5F67',
+            borderColor: '#B42867',
+            data: [200, 50, 250, 300, 156, 232, 80],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+
+    }
+});
+
+var ctx = document.getElementById('mobile_users_chart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ["Phones", "Tablets", "Desktop"],
+        datasets: [{
+            backgroundColor: ['#2A5F67','#B42867', '#C0B76B'],
+            data: [200, 50, 250],
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+
+    }
+});
